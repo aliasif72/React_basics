940b10723b76d7e13dd565884195f73e1227afe7
aliasif72 <aa2061996@gmail.com>
2023-12-14 21:24:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Expense from './components/ExpenseItems'
 
 function App() {
+  const dateOf=new Date();
+  const arr=[{title:"Movies", price:"Rs 200", locationOfExpense:"Delhi"},
+  {title:"Medicines", price:"Rs 100",  locationOfExpense:"Bangaluru"},
+  {title:"Soda", price:"Rs 20", locationOfExpense:"Lucknow"}]
   return (
     <>
-    <h2>React App Expense</h2>
-    <Expense/>
-    
-    </>
- 
-      );
+    <h1>Expense Tracker</h1>
+       {
+       arr.map(ele=>
+        {
+       return <Expense title={ele.title} price={ele.price} date={dateOf} locationOfExpense={ele.locationOfExpense}/>  
+        })
+        }
+         </>
+       );
   }
 
 
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,15 +1,23 @@
-function Expense()
+import './ExpenseItems.css';
+function Expense(props)
 {
-    return (
-        <>
-        <h3>Expense Items</h3>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
-        </>
-        
-    )
-
+    const month= props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.getDate();
+    const year=props.date.getFullYear();
+        return (
+        <div className="expense-item">
+            <div>
+                <div>{month} </div>
+                <div>{day} </div>
+                <div>{year} </div>
+                </div>
+                <div className="expense-item__description">
+                <h3>{props.title}</h3>
+                  <div className="expense-item__price">{props.price}</div>
+                  <div className='expense-item__price'> {props.locationOfExpense}</div>
+                </div>
+             </div>   
+        )
 }
 
 export default Expense
