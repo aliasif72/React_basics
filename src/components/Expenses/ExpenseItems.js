de89eba35c14d2e9bc53192fa9c07b6bc1419dde
aliasif72 <aa2061996@gmail.com>
2023-12-15 01:35:27 +0530

    7th used state

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,21 +1,25 @@
+import {useState} from 'react';
 import './ExpenseItems.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 function Expense(props)
 {
-  const clickHandler=()=>{
-    
-  }
+   const clickHandler=()=>{
+    setDis(" ");
+      }
+  const disp=<Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+           <div className="expense-item__description">
+            <h3>{props.title}</h3>
+              <div className="expense-item__price">{props.price}</div>
+              <div className="expense-item__price"> {props.locationOfExpense}</div>
+            </div>
+            <button className="expense-item__button"  onClick={clickHandler}>Delete</button>
+         </Card>   
+   const[dis,setDis] = useState(disp);
+          
         return (
-        <Card className="expense-item">
-          <ExpenseDate date={props.date}/>
-               <div className="expense-item__description">
-                <h3>{props.title}</h3>
-                  <div className="expense-item__price">{props.price}</div>
-                  <div className="expense-item__price"> {props.locationOfExpense}</div>
-                </div>
-                <button className="expense-item__button"  onClick={clickHandler}>Delete</button>
-             </Card>   
+        dis
         )
 }
 
