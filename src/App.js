ae1d9c9fef51dd1efb5463ec3afe758afa879db6
aliasif72 <aa2061996@gmail.com>
2023-12-14 16:17:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Expense from './components/ExpenseItems'
+
+function App() {
+  return (
+    <>
+    <h2>React App Expense</h2>
+    <Expense/>
+    
+    </>
+ 
+      );
+  }
+
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,15 @@
+function Expense()
+{
+    return (
+        <>
+        <h3>Expense Items</h3>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+        </>
+        
+    )
+
+}
+
+export default Expense
