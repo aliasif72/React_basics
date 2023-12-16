a1d1b8ef380b21af521bd05ce205e85503622d8b
aliasif72 <aa2061996@gmail.com>
2023-12-16 05:42:21 +0530

    9th form submit with/out state

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,20 +1,55 @@
 import './NewExpense.css';
 import './ExpenseForm.css';
+//import {useState} from 'react';
 function ExpenseForm(){
+    // const[userInput, setUserInput]=useState({
+    //     enteredTitle:'',
+    //     enteredDate:'',
+    //     enteredAmount:''
+    // })
+    // function titleChanger(event)
+    // {
+    //     setUserInput((prevState)=>{
+    //     return {...prevState,enteredTitle:event.target.value}
+    //     })
+        
+    // }
+    // function dateChanger(event)
+    // {
+    //     setUserInput((prevState)=>{
+    //      return {...prevState,enteredDate:event.target.value}
+    //     })
+    //         }
+    // function amountChanger(event)
+    // {
+    //     setUserInput((prevState)=>{
+    //     return {...prevState,enteredAmount:event.target.value}
+    //     })
+    //      }
+         function submitHandler(event)
+         {
+            event.preventDefault();
+           const userInput={
+            title:event.target.title.value,
+            amount:event.target.amount.value,
+            date:event.target.date.value
+           }
+            console.log(userInput);
+        }
       return(
-        <form>         
+        <form onSubmit={submitHandler} >         
         <div className="new-expense__controls">
         <div className="new-expense__control">
-            <label>Date :  <input type="date"/></label>
+            <label>Date :  <input name="date" type="date"/></label>
             </div>
             <div className="new-expense__control">
-           <label> Title : <input  type="text"/></label>
+           <label> Title : <input name="title" type="text"/></label>
            </div>
            <div className="new-expense__control">        
-            <label> Amount : <input type="text" /></label>
+            <label> Amount : <input name="amount"  type="text" /></label>
             </div>
             <div className="new-expense__actions">
-            <button className="new-expense button" type="submit">Add Expense</button>
+            <button  className="new-expense button" type="submit">Add Expense</button>
             </div>
         </div>
         </form>
