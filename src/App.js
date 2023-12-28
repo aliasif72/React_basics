550536bc4134caec83fcc93a285573eb6619ce3c
aliasif72 <aa2061996@gmail.com>
2023-12-29 02:40:03 +0530

    15th first project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import './App.css';
-import NewExpense from './components/Expenses/NewExpense';
-import Expenses from './components/Expenses/Expenses';
-import { useState } from 'react';
-
-const dummyExpense=[{id:"e1",title:"Movies", amount:200 ,date:new Date()},
-{id:"e2", title:"Medicines", amount:100 , date:new Date()},
-{id:"e3", title:"Soda", amount:20, date:new Date()}];
-let num=4;
+import Form from './Components/Form'
 
 function App() {
-  const[expenseArr,setExpenseArr]=useState(dummyExpense);
-  function saveExpense(data)
-  {
-    data.id="e"+ num++;
-    setExpenseArr((prevExpenseArr)=>{
-    return [data,...prevExpenseArr]
-  })
-      
-}
-  return (
-    <div className='app-items'> 
-    <h1>Expense Tracker</h1>
-    <NewExpense onsaveExpense={saveExpense}/>
-       {
-        <Expenses items={expenseArr}/>  
-              }
-         </div>
-       );
-  }
-export default App;
 
+  const arr=[];
+
+  function addData(data){
+    arr.push(data);
+    console.log(arr);
+      }
+
+  return (
+       <Form prop={addData}/>
+  );
+}
+
+export default App;
